ba2f1a7abe6ee7ca37c2ba0c307bbea16ad9a485
Aleksandra Mayarovskaya <57211690+Aleksandra-ui@users.noreply.github.com>
2021-04-18 10:23:56 +0300

    Delete src/jwd directory

--- src/jwd/model/FigureType.java
+++
@@ -1,5 +0,0 @@
-package com.epam.jwd.model;
-
-public enum FigureType {
-	RECTANGLE, PENTAGON, HEXAGON
-}
--- src/jwd/service/FigurePreProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.epam.jwd.service;
-
-import java.util.List;
-import com.epam.jwd.model.FigureType;
-import com.epam.jwd.model.Point;
-
-public interface FigurePreProcessor {
-	boolean valid(FigureType type, List<Point> points);
-}
